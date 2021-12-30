80f299f483bef8c79501f8bd1c62c9f0d57cc7ee
Jubaear Islam Fahim <jubaearislam86664@gmail.com>
2021-12-30 11:23:34 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,449 @@
+(function ($) {
+    'use strict'
+
+    /*
+    ========================================
+    Top Nav
+    ========================================
+    */
+    $(window).on('scroll', function () {
+        var wScroll = $(this).scrollTop();
+        if (wScroll > 1) {
+            $('#topNab').addClass('nabActive');
+        } else {
+            $('#topNab').removeClass('nabActive');
+        };
+    });
+
+    $(document).ready(function () {
+        AOS.init();
+    });
+
+    /*
+    ========================================
+    Top Nav
+    ========================================
+    */
+    $(document).ready(function () {
+        $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+        });
+    });
+
+    $(".accordion-faq").accordion();
+
+
+
+    /*
+    ========================================
+    Slider Services
+    ========================================
+    */
+    /* if ($('.services-slider').length) {
+        $('.services-slider-nav').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            asNavFor: '.services-slider-for',
+            dots: false,
+            focusOnSelect: true,
+            autoplay: false,
+        });
+        $('.services-slider-for').slick({
+            loop: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+        });
+    } */
+
+
+    /*
+    ========================================
+    
+    ========================================
+    */
+    $(document).ready(function () {
+
+        $('#bar-1').jqbar({ label: 'Link', value: 30, orientation: 'v' });
+
+        $('#bar-2').jqbar({ label: 'On-Page', value: 70, orientation: 'v' });
+
+        $('#bar-3').jqbar({ label: 'Behavioral', value: 50, orientation: 'v' });
+
+        $('#bar-4').jqbar({ label: 'GMB', value: 90, orientation: 'v' });
+
+        $('#bar-5').jqbar({ label: 'Citation', value: 70, orientation: 'v' });
+
+        $('#bar-6').jqbar({ label: 'Personalization', value: 50, orientation: 'v' });
+
+        $('#bar-7').jqbar({ label: 'Review', value: 70, orientation: 'v' });
+
+        $('#bar-8').jqbar({ label: 'Social', value: 80, orientation: 'v' });
+
+
+    });
+    /*
+    ========================================
+    Services Tab
+    ========================================
+    */
+    $('.classes-tabs li').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('.classes-tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    });
+
+
+    /* --------------------------- */
+    /*  6. imagesLoaded JS
+    /* --------------------------- */
+    $('#container').imagesLoaded(function () {
+        var $grid = $('.portfolio-grid').isotope({
+            itemSelector: '.portfolio-grid-item',
+            percentPosition: true,
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: '.portfolio-grid-item',
+            }
+        });
+
+        // filter items on button click
+        $('.filter-button-group').on('click', 'button', function () {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+        });
+
+        //for menu active class
+        $('.portfolio-button button').on('click', function (event) {
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            event.preventDefault();
+        });
+    });
+
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        /////// Prevent closing from click inside dropdown
+        document.querySelectorAll('.dropdown-menu').forEach(function (element) {
+            element.addEventListener('click', function (e) {
+                e.stopPropagation();
+            });
+        })
+    });
+
+
+    $(document).ready(function () {
+
+        $('.barfiller').barfiller();
+
+    });
+
+    $('.chosen-select').chosen();
+    $('.chosen-select-deselect').chosen({ allow_single_deselect: true });
+
+
+
+    $(document).ready(function () {
+
+        var current_fs, next_fs, previous_fs; //fieldsets
+        var opacity;
+
+        $(".next").click(function () {
+
+            current_fs = $(this).parent();
+            next_fs = $(this).parent().next();
+
+            //Add Class Active
+            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+            //show the next fieldset
+            next_fs.show();
+            //hide the current fieldset with style
+            current_fs.animate({ opacity: 0 }, {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                    next_fs.css({ 'opacity': opacity });
+                },
+                duration: 600
+            });
+        });
+
+        $(".previous").click(function () {
+
+            current_fs = $(this).parent();
+            previous_fs = $(this).parent().prev();
+
+            //Remove class active
+            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+            //show the previous fieldset
+            previous_fs.show();
+
+            //hide the current fieldset with style
+            current_fs.animate({ opacity: 0 }, {
+                step: function (now) {
+                    // for making fielset appear animation
+                    opacity = 1 - now;
+
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                    previous_fs.css({ 'opacity': opacity });
+                },
+                duration: 600
+            });
+        });
+
+        $('.radio-group .radio').click(function () {
+            $(this).parent().find('.radio').removeClass('selected');
+            $(this).addClass('selected');
+        });
+
+        $(".submit").click(function () {
+            return false;
+        })
+
+    });
+    /*
+    ========================================
+    Scroll to top
+    ========================================
+    */
+    if ($('#scroll-top').length) {
+        function scrollToTop() {
+            var $scrollUp = $('#scroll-top'),
+                $lastScrollTop = 0,
+                $window = $(window);
+
+            $window.on('scroll', function () {
+                var st = $(this).scrollTop();
+                if (st > $lastScrollTop) {
+                    $scrollUp.removeClass('show');
+                } else {
+                    if ($window.scrollTop() > 200) {
+                        $scrollUp.addClass('show');
+                    } else {
+                        $scrollUp.removeClass('show');
+                    }
+                }
+                $lastScrollTop = st;
+            });
+
+            $scrollUp.on('click', function (evt) {
+                $('html, body').animate({ scrollTop: 0 }, 600);
+                evt.preventDefault();
+            });
+        }
+        scrollToTop();
+    }
+
+
+
+    $('.projects-slid').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+    });
+
+
+
+
+    jQuery(document).ready(function ($) {
+        //set animation timing
+        var animationDelay = 2500,
+            //loading bar effect
+            barAnimationDelay = 3800,
+            barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+            //letters effect
+            lettersDelay = 50,
+            //type effect
+            typeLettersDelay = 150,
+            selectionDuration = 500,
+            typeAnimationDelay = selectionDuration + 800,
+            //clip effect 
+            revealDuration = 600,
+            revealAnimationDelay = 1500;
+
+        initHeadline();
+
+
+        function initHeadline() {
+            //insert <i> element for each letter of a changing word
+            singleLetters($('.cd-headline.letters').find('b'));
+            //initialise headline animation
+            animateHeadline($('.cd-headline'));
+        }
+
+        function singleLetters($words) {
+            $words.each(function () {
+                var word = $(this),
+                    letters = word.text().split(''),
+                    selected = word.hasClass('is-visible');
+                for (i in letters) {
+                    if (word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+                    letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>' : '<i>' + letters[i] + '</i>';
+                }
+                var newLetters = letters.join('');
+                word.html(newLetters).css('opacity', 1);
+            });
+        }
+
+        function animateHeadline($headlines) {
+            var duration = animationDelay;
+            $headlines.each(function () {
+                var headline = $(this);
+
+                if (headline.hasClass('loading-bar')) {
+                    duration = barAnimationDelay;
+                    setTimeout(function () { headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+                } else if (headline.hasClass('clip')) {
+                    var spanWrapper = headline.find('.cd-words-wrapper'),
+                        newWidth = spanWrapper.width() + 10
+                    spanWrapper.css('width', newWidth);
+                } else if (!headline.hasClass('type')) {
+                    //assign to .cd-words-wrapper the width of its longest word
+                    var words = headline.find('.cd-words-wrapper b'),
+                        width = 0;
+                    words.each(function () {
+                        var wordWidth = $(this).width();
+                        if (wordWidth > width) width = wordWidth;
+                    });
+                    headline.find('.cd-words-wrapper').css('width', width);
+                };
+
+                //trigger animation
+                setTimeout(function () { hideWord(headline.find('.is-visible').eq(0)) }, duration);
+            });
+        }
+
+        function hideWord($word) {
+            var nextWord = takeNext($word);
+
+            if ($word.parents('.cd-headline').hasClass('type')) {
+                var parentSpan = $word.parent('.cd-words-wrapper');
+                parentSpan.addClass('selected').removeClass('waiting');
+                setTimeout(function () {
+                    parentSpan.removeClass('selected');
+                    $word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
+                }, selectionDuration);
+                setTimeout(function () { showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
+
+            } else if ($word.parents('.cd-headline').hasClass('letters')) {
+                var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+                hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+                showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+
+            } else if ($word.parents('.cd-headline').hasClass('clip')) {
+                $word.parents('.cd-words-wrapper').animate({ width: '2px' }, revealDuration, function () {
+                    switchWord($word, nextWord);
+                    showWord(nextWord);
+                });
+
+            } else if ($word.parents('.cd-headline').hasClass('loading-bar')) {
+                $word.parents('.cd-words-wrapper').removeClass('is-loading');
+                switchWord($word, nextWord);
+                setTimeout(function () { hideWord(nextWord) }, barAnimationDelay);
+                setTimeout(function () { $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+
+            } else {
+                switchWord($word, nextWord);
+                setTimeout(function () { hideWord(nextWord) }, animationDelay);
+            }
+        }
+
+        function showWord($word, $duration) {
+            if ($word.parents('.cd-headline').hasClass('type')) {
+                showLetter($word.find('i').eq(0), $word, false, $duration);
+                $word.addClass('is-visible').removeClass('is-hidden');
+
+            } else if ($word.parents('.cd-headline').hasClass('clip')) {
+                $word.parents('.cd-words-wrapper').animate({ 'width': $word.width() + 10 }, revealDuration, function () {
+                    setTimeout(function () { hideWord($word) }, revealAnimationDelay);
+                });
+            }
+        }
+
+        function hideLetter($letter, $word, $bool, $duration) {
+            $letter.removeClass('in').addClass('out');
+
+            if (!$letter.is(':last-child')) {
+                setTimeout(function () { hideLetter($letter.next(), $word, $bool, $duration); }, $duration);
+            } else if ($bool) {
+                setTimeout(function () { hideWord(takeNext($word)) }, animationDelay);
+            }
+
+            if ($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+                var nextWord = takeNext($word);
+                switchWord($word, nextWord);
+            }
+        }
+
+        function showLetter($letter, $word, $bool, $duration) {
+            $letter.addClass('in').removeClass('out');
+
+            if (!$letter.is(':last-child')) {
+                setTimeout(function () { showLetter($letter.next(), $word, $bool, $duration); }, $duration);
+            } else {
+                if ($word.parents('.cd-headline').hasClass('type')) { setTimeout(function () { $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200); }
+                if (!$bool) { setTimeout(function () { hideWord($word) }, animationDelay) }
+            }
+        }
+
+        function takeNext($word) {
+            return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+        }
+
+        function takePrev($word) {
+            return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+        }
+
+        function switchWord($oldWord, $newWord) {
+            $oldWord.removeClass('is-visible').addClass('is-hidden');
+            $newWord.removeClass('is-hidden').addClass('is-visible');
+        }
+    });
+
+
+
+
+})(jQuery);
